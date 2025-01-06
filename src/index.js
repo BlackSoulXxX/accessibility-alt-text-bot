805f2fb5b051c0ba5251a385610f270f48e173c8
BlackSoulXxX <BlackSoul7018@hotmail.com>
2025-01-07 02:09:37 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { validate } from "./validate.js";
 const [content] = process.argv.slice(2);
 
 const run = async () => {
-  console.log((await validate(content)).length > 0);
+  console.log((await validate(content)).length >
 };
 
 run();
